b41edc9b78dbf557228dc503180e84bd06f28455
AlexandreSeptembreYnov <you@example.com>
2020-01-01 10:41:51 +0100

    init et display

---
+++ index.js
@@ -0,0 +1,12 @@
+class Personnage {
+     constructor(Force, Dexterite, Intelligence, Vie) {
+        this.Force = Force;
+        this.Dexterite = Dexterite;
+        this.Intelligence = Intelligence;
+        this.Vie = Vie;
+     }
+     moinVie(pv){
+            
+     }
+
+}
